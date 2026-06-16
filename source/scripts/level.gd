2a83124f5bb59963dafd072a072c3904e708766d
Stephen Frister <stephen.frister@hotmail.com>
2026-06-16 16:03:51 -0700

    Initial Click and Drag

--- source/scripts/level.gd
+++ source/scripts/level.gd
@@ -2,8 +2,19 @@ class_name Level
 extends Node2D
 
 
+#@onready var _mouse_object : MouseObject = $MouseObject
+
 signal return_to_menu
 
+#var mouse_last_position : Vector2 = Vector2(0.0,0.0)
+#
+
+
+func _on_move_mouse_object( _mouse_new_position : Vector2 ): 
+	
+	$MouseObject.position = _mouse_new_position
+	
+	pass
 
 func _on_main_menu_pressed() -> void:
 	
@@ -14,5 +25,15 @@ func _on_main_menu_pressed() -> void:
 func _on_restart_pressed() -> void:
 	
 	print_debug("currently does nothing...")
+	
+	pass
+
+func _on_mouse_clicked(is_clicked: bool, _mouse_position: Vector2) -> void:
+	print_debug( "_on_mouse_clicked: ")
+	
+	if is_clicked: $MouseObject.show()
+	if !is_clicked: $MouseObject.hide()
+	
+		#$MouseObject.position = mouse_position
 	
 	pass
