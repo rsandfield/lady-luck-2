5e5efd7556ddf427865a4cf2951be9ca4e0596f0
Stephen Frister <stephen.frister@hotmail.com>
2026-06-18 11:47:02 -0700

    Squashed commit of the following:
    commit d3149c0fcdcb37c089025e69463d232f06ba05be
    Author: Robert Sandfield <robert.f.sandfield@gmail.com>
    Date:   Thu Jun 18 11:30:09 2026 -0700
    
        Add spinner for our Lady
    
    commit da02dd7b4b8136414a4535280fb0c693fd940d29
    Author: Robert Sandfield <robert.f.sandfield@gmail.com>
    Date:   Thu Jun 18 09:06:36 2026 -0700
    
        Add row flags to grid

---
+++ source/floor/wheel/pie_slice.gd
@@ -0,0 +1,31 @@
+@tool
+class_name PieSlice
+extends Node2D
+
+
+@export var line_color: Color = Color.BLACK
+@export var fill_color: Color = Color.PURPLE
+@export var denominator: int = 4
+@export var line_width: float = 4
+@export var radius: float = 100
+
+
+# func _process(_delta: float):
+# 	queue_redraw()
+
+
+func _draw():
+	var arc := TAU / denominator
+	var edge = Vector2.UP * radius
+	var points: PackedVector2Array = [Vector2.ZERO]
+	var point_count: float = 16
+	var offset = 0
+	if denominator % 2 == 0:
+		offset = point_count * 0.5
+	for i in point_count + 1:
+		points.append(edge.rotated((i - offset) * arc / point_count))
+	points.append(Vector2.ZERO)
+	
+	
+	draw_colored_polygon(points, fill_color)
+	draw_polyline(points, line_color, line_width)
